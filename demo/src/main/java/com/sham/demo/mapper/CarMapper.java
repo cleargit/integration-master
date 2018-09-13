99b1c05d7d497fce992f01b8715319a6e582a699
sham <13172608381@163.com>
2018-09-13 17:38:35 +0800

    pulgin

---
+++ demo/src/main/java/com/sham/demo/mapper/CarMapper.java
@@ -0,0 +1,7 @@
+package com.sham.demo.mapper;
+
+import com.sham.demo.model.Car;
+import com.sham.mybatis.mymapper.BaseMapper;
+
+public interface CarMapper extends BaseMapper<Car> {
+}
---
+++ demo/src/main/java/com/sham/demo/model/Car.java
@@ -0,0 +1,72 @@
+package com.sham.demo.model;
+
+import com.sham.common.base.BaseModel;
+import javax.persistence.*;
+
+public class Car extends BaseModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String brand;
+
+    private String color;
+
+    private Integer year;
+
+    /**
+     * @return id
+     */
+    public Long getId() {
+        return id;
+    }
+
+    /**
+     * @param id
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    /**
+     * @return brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return color
+     */
+    public String getColor() {
+        return color;
+    }
+
+    /**
+     * @param color
+     */
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    /**
+     * @return year
+     */
+    public Integer getYear() {
+        return year;
+    }
+
+    /**
+     * @param year
+     */
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+}
