3546838ee82dc148f4be8d08b85a80b12f06e9ab
sham <13172608381@163.com>
2018-11-22 11:44:01 +0800

    空空如也

--- wxplat/src/main/java/com/sham/WxplatApplication.java
+++ wxplat/src/main/java/com/sham/WxplatApplication.java
@@ -2,8 +2,12 @@ package com.sham;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import tk.mybatis.spring.annotation.MapperScan;
 
+@MapperScan(basePackages = "com.sham.*.mapper")
 @SpringBootApplication
+@EnableCaching
 public class WxplatApplication {
 
     public static void main(String[] args) {
